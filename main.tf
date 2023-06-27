41ec997ba6597ed17bfb03380c239174c6bc56c8
havegrit <shin_jaean@naver.com>
2023-06-27 10:20:30 +0900

    configure cloud, region

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  cloud {
+    organization = "og-shin"
+    workspaces {
+      name = "ws-1"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "!> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
