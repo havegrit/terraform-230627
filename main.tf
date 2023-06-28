9bbc050648d7a512753b7975a4c0df07bb9cb4df
havegrit <shin_jaean@naver.com>
2023-06-28 10:24:33 +0900

    subnet mapping to rt_1

--- main.tf
+++ main.tf
@@ -56,3 +56,34 @@ resource "aws_route_table" "rt_1" {
     Name = "${var.prefix}-rt-1"
   }
 }
+
+resource "aws_route_table" "rt_2" {
+  vpc_id = aws_vpc.vpc_1.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw_1.id
+  }
+
+  tags = {
+    Name = "${var.prefix}-rt-2"
+  }
+}
+
+resource "aws_route_table" "rt_3" {
+  vpc_id = aws_vpc.vpc_1.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw_1.id
+  }
+
+  tags = {
+    Name = "${var.prefix}-rt-3"
+  }
+}
+
+resource "aws_route_table_association" "association_1" {
+  subnet_id      = aws_subnet.subnet_1.id
+  route_table_id = aws_route_table.rt_1.id
+}
